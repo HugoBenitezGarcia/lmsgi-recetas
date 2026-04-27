ef736c8ab4a0671d6af44d57cd72ca1631eaaeca
HugoBenitezGarcia <hbenagr0801@iesrafaelalberti.es>
2026-04-27 14:05:33 +0200

    js y xsd arreglado

--- js/app.js
+++ js/app.js
@@ -2,5 +2,32 @@ const cuerpoTabla = document.getElementById('cuerpo_tabla');
 async function cargarRecetas(){
     try {
         const respuesta = await fetch('recetas.xml')
+        const textoXml = await respuesta.text();
+        const parser = new DOMParser();
+        const xmlDOC = parser.parseFromString(textoXml, 'application/xml');
+        const recetas = xmlDoc.querySelectorAll('receta');
+
+        for (const receta of recetas) {
+            const fila = document.createElement('tr');
+            const codigo = receta.getAttribute('codigo');
+            const nombre = receta.querySelector('nombre').textContent;
+            const categoria = receta.querySelector('categoria');
+            const tiempo = receta.querySelector('tiempo').textContent;
+            const dificultad = receta.querySelector('dificultad').textContent;
+
+            fila.innerHTML = `
+            <td>${codigo}</td>
+            <td>${nombre}</td>
+            <td>${categoria}</td>
+            <td>${tiempo}</td>
+            <td>${dificultad}</td>
+            `;
+
+            cuerpoTabla.appendChild(fila);
+        }
+    } catch (error) {
+        console.error(error);
     }
 }
+
+cargarRecetas();
